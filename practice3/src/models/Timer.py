0477073895c3f74575564a1ec73e7f7dab94dce4
William MAILLARD <thomasjean228@gmail.com>
2023-10-26 15:00:19 +0200

    fix: improve Timer logic
    Can't start a new measure when one is already ongoing.
    Don't need to specify the name while calling stop.

--- practice3/src/models/Timer.py
+++ practice3/src/models/Timer.py
@@ -24,18 +24,35 @@ class Timer:
         return len(self.measure)
 
     def __str__(self) -> str:
-        raise NotImplementedError("Not implemented yet")
+        print(f"Timer with {len(self)} measures")
+        for name in self.measure:
+            print(f" - {name} : {self.computation_time(name, self.default_unit)} {self.default_unit.name}")
         
-    def start(self, name):
+    def start(self, name, force=False) -> bool:
+        if self.current is not None:
+            print("A timer is already started, stop it first.")
+            return False
+        
+        if name in self.measure and not force:
+            print("The name is already used, used force option to overwrite it.")
+            return False
+        
         self.measure[name] = [perf_counter_ns()]
+        self.current = name
         pass
 
-    def stop(self, name):
-        self.measure[name].append(perf_counter_ns())
-        pass
+    def stop(self):
+        if self.current is None:
+            return False
+        self.measure[self.current].append(perf_counter_ns())
+        self.current = None
+        return True
     
     def computation_time(self, name, unit=TimeUnit.S) -> float:
         return (self.measure[name][1] - self.measure[name][0])/unit.value
+    
+    def display_histogram(self):
+        raise NotImplementedError("Not implemented yet")
         
     
 

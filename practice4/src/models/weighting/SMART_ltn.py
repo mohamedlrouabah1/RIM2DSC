900c3b9ce4144a9bc7674ad328f9e143700b251e
William MAILLARD <thomasjean228@gmail.com>
2023-10-28 10:59:30 +0200

    feat: add caching
    Split the weighting function and add caching so that
    call with the same parameters are not computed again
    but rather take from the cache memory.

--- practice4/src/models/weighting/SMART_ltn.py
+++ practice4/src/models/weighting/SMART_ltn.py
@@ -1,11 +1,23 @@
+from functools import lru_cache
 from math import log10
 from WeightingFunction import WeightingFunction
 
 class SMART_ltn(WeightingFunction):
 
+    @lru_cache(maxsize=None)
+    def compute_idf(self, df, N):
+        return log10(N / df)
+    
+    @lru_cache(maxsize=None)
+    def compute_tf_part(self, tf):
+        return 1 + log10(tf)
+
     # TODO optimize this computation by computing the idf outside and not for each document 
+    @lru_cache(maxsize=None)
     def compute_score(self, tf, df, N):
         if df > 0 and N > df:
-            return (1 + log10(tf)) * log10(N / df)
+            idf = self.compute_idf(df, N)
+            tf_part = self.compute_tf_part(tf)
+            return tf_part * idf
         
         return 0

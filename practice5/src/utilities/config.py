b934c2ff60f283c4e6e9ea2cb4dab860084280a5
mohamedlrouabah1 <mohammed.rouabah@etu.univ-st-etienne.fr>
2023-11-17 19:49:13 +0100

    add : new branch

---
+++ practice5/src/utilities/config.py
@@ -0,0 +1,13 @@
+DATA_FOLDER="../../data/XML-Coll-withSem/"
+STOPWORDS_DIR = "../../stop-words/stop-words-english4.txt"
+SAVE_FOLDER="../../saves"
+RENDU_FOLDER="../../rendus"
+COLLECTION_FILES = [
+    'Text_Only_Ascii_Coll_NoSem'
+]
+GRAPH_FOLDER = "../graph/"
+
+COLLECTION_NAME = "Text_Only_Ascii_Coll_NoSem"
+
+# size of the ranking saved for a run
+NB_RANKING = 1500

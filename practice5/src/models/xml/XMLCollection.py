0b4f970f32af38d14321ede6d94194e75007b6f2
William MAILLARD <thomasjean228@gmail.com>
2023-12-07 23:49:50 +0100

    update: XMLDocument model

--- practice5/src/models/xml/XMLCollection.py
+++ practice5/src/models/xml/XMLCollection.py
@@ -1,7 +1,23 @@
 from models.concepts.CollectionOfRessources import CollectionOfRessources
-
+from models.xml.XMLDocument import XMLDocument
+from types import Any
 
 class XMLCollection(CollectionOfRessources):
 
-    def __init__(self, id:int, documents:dict):
+    def __init__(self, id:int, documents:dict(XMLDocument)):
         super().__init__(id, documents)
+
+    def load(self) -> Any:
+        raise NotImplementedError("Should implement load()")
+
+    def preprocess(self) -> None:
+        raise NotImplementedError("Should implement preprocessing()")
+
+    def index(self) -> None:
+        raise NotImplementedError("Should implement indexing()")
+    
+    def compute_RSV(self, query:str) -> dict(int):
+        raise NotImplementedError("Should implement query()")
+    
+    def compute_stats(self) -> dict(int):
+        raise NotImplementedError("Should implement compute_stats()")
--- practice5/src/models/xml/XMLDocument.py
+++ practice5/src/models/xml/XMLDocument.py
@@ -4,10 +4,8 @@ from models.xml.XMLElement import XMLElement
 class XMLDocument(InformationRessource):
 
     @classmethod
-    def create_xml_element(cls, xml_dict:dict) -> list(XMLElement):
+    def create_xml_elements(cls, xml_dict:dict) -> list(XMLElement):
         raise NotImplementedError("TODO!!")
 
     def __init__(self, id:int, preprocess_xml:dict):
-        super().__init__(id, None)
-        # to do create the XMLElement from the preprocess_xml dictionnary
-        self.content:XMLElement = XMLDocument.create_xml_element(preprocess_xml)
+        super().__init__(id, XMLDocument.create_xml_element(preprocess_xml))

80768eb9058493474ab9f34efd86dc52d3499610
William MAILLARD <thomasjean228@gmail.com>
2023-12-08 22:20:53 +0100

    fix: index function

--- practice5/src/models/xml/XMLIndexer.py
+++ practice5/src/models/xml/XMLIndexer.py
@@ -13,6 +13,6 @@ class XMLIndexer(TextIndexer):
         for field in xml_element.fields:
             self._index_text(field)
 
-    def index(self, xml_documents:list(XMLDocument), use_parallel_computing=False) -> None:
+    def index(self, xml_documents:list[XMLDocument], use_parallel_computing=False) -> None:
         for xml_doc in tqdm(xml_documents, desc="Indexing XML documents", file=stderr):
             self._index_fields(xml_doc.content)

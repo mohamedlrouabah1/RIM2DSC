3265b7c20e30c9c692858b1570876932249117db
William MAILLARD <thomasjean228@gmail.com>
2023-12-08 18:52:40 +0100

    fix: Collection contains a list of XMLDocument
    instead of dict

--- practice5/src/models/xml/XMLCollection.py
+++ practice5/src/models/xml/XMLCollection.py
@@ -1,17 +1,42 @@
+from sys import stderr
+from types import Any
+
+import xml.dom.minidom as minidom
+
 from models.concepts.CollectionOfRessources import CollectionOfRessources
 from models.xml.XMLDocument import XMLDocument
-from types import Any
+from models.xml.XMLIndexer import XMLIndexer
+from models.xml.XMLPreprocessor import XMLPreprocessor
+from models.Timer import Timer
 
 class XMLCollection(CollectionOfRessources):
 
-    def __init__(self, id:int, documents:dict(XMLDocument)):
-        super().__init__(id, documents)
+    def __init__(self, path:str, indexer=None, preprocessor=None, use_parallel_computing=False):
+        super().__init__(path, {})
+        self.documents:list(XMLDocument) = []
+        self.terms_frequency:dict(str, int) = {}
+        self.vocabulary_size = 0
+        self.path = path
+        self.Timer = Timer()
+        self.preprocessor = XMLPreprocessor() if preprocessor is None else preprocessor
+        self.indexer = XMLIndexer() if indexer is None else indexer
+        self.information_retriever = None
+        self.use_parallel_computing = use_parallel_computing
 
-    def load(self) -> Any:
-        raise NotImplementedError("Should implement load()")
+    def load(self) -> list(tuple(str, minidom.Document)):
+        print(f"Loading collection from file {self.path} ...", file=stderr)
+        self.Timer.start("load_collection")
+        raw_xml_collection = self.preprocessor.load(self.path)
+        self.Timer.stop()
+        print(f"Collection loaded in {self.Timer.get_time('load_collection')} seconds.", file=stderr)
+        return raw_xml_collection
 
-    def preprocess(self) -> None:
-        raise NotImplementedError("Should implement preprocessing()")
+    def preprocess(self, raw_collection) -> None:
+        print("Preprocessing collection...", file=stderr)
+        self.Timer.start("preprocessing")
+        self.documents = self.preprocessor.pre_process(raw_collection, self.use_parallel_computing)
+        self.Timer.stop()
+        print(f"Collection preprocessed in {self.Timer.get_time('preprocessing')} seconds.", file=stderr)
 
     def index(self) -> None:
         raise NotImplementedError("Should implement indexing()")

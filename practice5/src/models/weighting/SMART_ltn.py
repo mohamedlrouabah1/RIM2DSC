3d16a9fe5166d983c4d7f13ab0d79d507f90fff8
William MAILLARD <thomasjean228@gmail.com>
2023-12-11 12:06:53 +0100

    fix: remove the cache to try to please the OOM Killer

--- practice5/src/models/weighting/SMART_ltn.py
+++ practice5/src/models/weighting/SMART_ltn.py
@@ -13,11 +13,11 @@ class SMART_ltn(WeightingFunction):
         super().__init__()
         self.N = N
 
-    @lru_cache(maxsize=None)
+    #@lru_cache(maxsize=None)
     def compute_idf(self, df, N):
         return log10(N / df)
     
-    @lru_cache(maxsize=None)
+    #@lru_cache(maxsize=None)
     def compute_tf_part(self, tf):
         return 1 + log10(tf) if tf > 0 else 0
 

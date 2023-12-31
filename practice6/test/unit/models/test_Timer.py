71545643aca332719522d427d3d0a29e7875444d
William MAILLARD <thomasjean228@gmail.com>
2023-12-31 15:08:01 +0100

    test: Timer

--- practice6/test/unit/models/test_Timer.py
+++ practice6/test/unit/models/test_Timer.py
@@ -1,17 +1,52 @@
 import time
 import unittest
-
-from models.Timer import Timer
+from models.Timer import *
 
 class TestTimer(unittest.TestCase):
 
+    def test_timer_init(self):
+        # Test Timer initialization
+        timer = Timer()
+        self.assertIsNotNone(timer)
+        self.assertEqual(len(timer), 0)
 
-    def time_measure(self, timer:Timer, id:str, sleep_time:float):
-        timer.start(id)
+    def test_time_measurement(self):
+        # Test time measurement using Timer
+        timer = Timer()
+        function_id = "test_function"
+        sleep_time = 0.1  # in seconds
+
+        # Measure the execution time of a function
+        self.time_measure(timer, function_id, sleep_time)
+
+        # Verify that the measurement has been added
+        self.assertEqual(len(timer), 1)
+
+        # Verify that the time is recorded in nanoseconds by default
+        time_ns = timer.measure[function_id][1] - timer.measure[function_id][0]
+        self.assertAlmostEqual(timer.get_time(function_id), time_ns, delta=1e6)  # Delta tolerance for rounding errors
+
+        # Verify that the time is correctly converted to milliseconds
+        time_ms = time_ns / TimeUnit.MS.value
+        self.assertAlmostEqual(timer.get_time(function_id, TimeUnit.MS), time_ms, delta=1e-3)
+
+        # Verify that the time is correctly converted to seconds
+        time_s = time_ns / TimeUnit.S.value
+        self.assertAlmostEqual(timer.get_time(function_id, TimeUnit.S), time_s, delta=1e-3)
+
+        # Verify that the time is correctly converted to minutes
+        time_min = time_ns / TimeUnit.MIN.value
+        self.assertAlmostEqual(timer.get_time(function_id, TimeUnit.MIN), time_min, delta=1e-3)
+
+        # Verify that the time is correctly converted to hours
+        time_hour = time_ns / TimeUnit.HOUR.value
+        self.assertAlmostEqual(timer.get_time(function_id, TimeUnit.HOUR), time_hour, delta=1e-3)
+
+    def time_measure(self, timer: Timer, function_id: str, sleep_time: float):
+        # Helper method to measure time for testing
+        timer.start(function_id)
         time.sleep(sleep_time)
         timer.stop()
 
-
-    def test_timer(self):
-        timer = Timer()
-        assert timer is not None
+if __name__ == "__main__":
+    unittest.main()

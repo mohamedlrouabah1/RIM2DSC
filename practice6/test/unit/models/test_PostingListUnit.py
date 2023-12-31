4528670548582536093c5fc8ec6454d419e983a3
William MAILLARD <thomasjean228@gmail.com>
2023-12-31 15:07:47 +0100

    test: PostingListUnit

--- practice6/test/unit/models/test_PostingListUnit.py
+++ practice6/test/unit/models/test_PostingListUnit.py
@@ -1,8 +1,25 @@
 import unittest
-
 from models.PostingListUnit import PostingListUnit
 
 class TestPostingListUnit(unittest.TestCase):
-        
+
     def test_init(self):
-        pass
+        """Test the initialization of PostingListUnit"""
+        document_id, frequency = 1, 2.5
+        posting_unit = PostingListUnit(document_id, frequency)
+
+        self.assertEqual(posting_unit.document_id, document_id)
+        self.assertEqual(posting_unit.frequency, frequency)
+
+    def test_str(self):
+        """ Test the __str__ method of PostingListUnit"""
+        document_id = 1
+        frequency = 2.5
+        posting_unit = PostingListUnit(document_id, frequency)
+
+        # Verify the string representation
+        expected_str = f"Document ID: {document_id} - Frequency: {frequency}"
+        self.assertEqual(str(posting_unit), expected_str)
+
+if __name__ == "__main__":
+    unittest.main()

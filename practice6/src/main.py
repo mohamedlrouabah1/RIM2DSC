ff1591a0e636a09b3057a24b8ab1e09b008bd6d6
William MAILLARD <thomasjean228@gmail.com>
2023-12-31 15:59:48 +0100

    fix: update main according to recent changes

--- practice6/src/main.py
+++ practice6/src/main.py
@@ -6,9 +6,9 @@ from models.weighting.BM25 import BM25
 from models.weighting.SMART_ltc import SMART_ltc
 from models.weighting.SMART_ltn import SMART_ltn
 from models.weighting.SMART_lnu import SMART_lnu
-from utilities.config import NB_RANKING, RECURSION_LIM
+from utilities.config import RECURSION_LIM
 from utilities.parser import parse_command_line_arguments
-from utilities.utilities import create_or_load_collection, load_queries_from_csv
+from utilities.utilities import create_or_load_collection, load_queries_from_csv, launch_run
 
 def main() -> None:
     # Process program's arguments
@@ -42,85 +42,9 @@ def main() -> None:
     print("Loading queries...", file=sys.stderr)
     queries = load_queries_from_csv(args.queries_file_path)
     
-
     # To create run result files
-    print("Instanciate IRun class ...", file=sys.stderr)
-    run = IRrun(
-        weighting_function=args.ranking,
-        stop=args.stopword,
-        stem=args.stemmer,
-        params=params,
-    )
-
-    # for the display
-    delimiter = "-" * 80
-    top_n = args.top_n
-    
-    for id, query in queries:
-        id = int(id)
-        print(f"Query: {query}")
-        collection.Timer.start(f"query{id:02d}_preprocessing")
-        query = collection.preprocessor._text_preprocessing(query)
-        collection.Timer.stop()
-        print(f"Query preprocessed in {collection.Timer.get_time(f'query{id:02d}_preprocessing')}")
-        print(f"Query preprocessed: {query}")
-        print(delimiter)
-
-        print(f"Ranking documents...")
-        collection.Timer.start(f"query{id:02d}_ranking")
-        ranking = collection.compute_RSV(query)
-        collection.Timer.stop()
-        print(f"Documents ranked in {collection.Timer.get_time(f'query{id:02d}_ranking')}")
-        print(delimiter)
-
-        # we filter overlapping results
-        nb_scores = 0
-        run_lines = []
-        doc_already_return = set()
-        run_lines.append(ranking[0])
-        j = 1 
-        doc_already_return.add(ranking[0][0].split(':')[0])
-        while nb_scores < NB_RANKING  and j < len(ranking):
-            line_id, line_xpath = run_lines[nb_scores][0].split(':')
-            doc_id, xpath = ranking[j][0].split(':')
-
-            # does it overlap with the previous score ?
-            if line_id == doc_id  and xpath.find(line_xpath) != -1:
-                run_lines[nb_scores] = ranking[j]
-
-            
-            # does it not intertwine with a previous result return from the document ?
-            elif doc_id not in doc_already_return:
-                nb_scores += 1
-                if nb_scores < NB_RANKING:
-                    run_lines.append(ranking[j])
-                    doc_already_return.add(doc_id)
-
-            j+=1
-                
-        if j < NB_RANKING:
-            print(f"Only {j} results for query {id} instead of {NB_RANKING}")
-
-
-        print(f"Ranking results:")
-        for i, (doc_id, score) in enumerate(run_lines[:top_n]):
-            print(f"#{i+1} - Document {doc_id} with score {score}")
-        print(delimiter)
-        print("\n\n")
-
-
-        # We add the results to the run file
-        for i, (doc_id, score) in enumerate(run_lines):
-            # xml_path = Document.get_xml_path()
-            run.add_result_line(
-                query_id=id,
-                doc_id=doc_id,
-                rank=i+1,
-                score=score,
-            )
-
-    # Finnally we save the run file
-    run.save_run(verbose=True)
+    launch_run(collection, queries, args.index_path, args.ranking, params)
+   
     
 if __name__ == "__main__":
     sys.setrecursionlimit(RECURSION_LIM)

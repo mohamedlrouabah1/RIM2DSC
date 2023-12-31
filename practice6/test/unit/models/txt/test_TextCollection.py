81cc4220933e75dfd69372e69020ccce92664338
William MAILLARD <thomasjean228@gmail.com>
2023-12-31 15:40:16 +0100

    test: TextCollection

--- practice6/test/unit/models/txt/test_TextCollection.py
+++ practice6/test/unit/models/txt/test_TextCollection.py
@@ -1,8 +1,16 @@
 import unittest
-
 from models.txt.TextCollection import TextCollection
 
 class TestTextCollection(unittest.TestCase):
-        
-        def test_init(self):
-            pass
+
+    COLL_PATH = "../../data/Practice_05_data/XML-Coll-withSem/"
+
+    def setUp(self):
+        self.text_collection = TextCollection(path=TestTextCollection.COLL_PATH)
+
+    def test_init(self):
+        self.assertIsInstance(self.text_collection, TextCollection)
+        self.assertEqual(self.text_collection.path, TestTextCollection.COLL_PATH)
+
+if __name__ == "__main__":
+    unittest.main()

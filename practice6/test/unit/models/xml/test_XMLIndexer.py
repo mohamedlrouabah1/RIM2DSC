fa387e5b1806167b494e3a9e7aa2337eaf8a7b51
William MAILLARD <thomasjean228@gmail.com>
2023-12-31 15:20:37 +0100

    test: XMLIndexer

--- practice6/test/unit/models/xml/test_XMLIndexer.py
+++ practice6/test/unit/models/xml/test_XMLIndexer.py
@@ -1,8 +1,13 @@
 import unittest
-
 from models.xml.XMLIndexer import XMLIndexer
 
 class TestXMLIndexer(unittest.TestCase):
-        
+
+    def setUp(self):
+        self.xml_indexer = XMLIndexer()
+
     def test_init(self):
-        pass    
+        self.assertIsInstance(self.xml_indexer, XMLIndexer)
+
+if __name__ == "__main__":
+    unittest.main()

553d53f3c1e72386ee3b405a4b8d316f40e353a0
William MAILLARD <thomasjean228@gmail.com>
2024-01-04 11:18:06 +0100

    feat: graph creation

--- practice6/src/models/network/PageRank.py
+++ practice6/src/models/network/PageRank.py
@@ -1,6 +1,20 @@
 class PageRank():
 
-    def pagerank(cls, graph, d=0.85, max_iterations=100, convergence_threshold=1e-6):
+    def __init__(self, list_tag_link:list[str, str, list[str]]):
+        """ 
+        create the graph of links from a list of tag links.
+        Params:
+        -------
+        list_tag_link: (list[str, str, list[str]])
+            list of tuples (doc_id, reffered_doc_id, anchor_tokens)
+        """
+        self.graph = dict()
+        for doc_id, referred_doc_id, _ in list_tag_link:
+            if doc_id not in self.graph:
+                self.graph[doc_id] = []
+            self.graph[doc_id].append(referred_doc_id)
+
+    def pagerank(self, d=0.85, max_iterations=100, convergence_threshold=1e-6):
         """
         Calculate PageRank scores for a given graph.
 
@@ -10,24 +24,24 @@ class PageRank():
         - d (float, optional): Damping factor, typically between 0 and 1. It represents the probability that a user
                             will follow a link. Defaults to 0.85.
         - max_iterations (int, optional): Maximum number of iterations for the PageRank calculation. Defaults to 100.
-        - convergence_threshold (float, optional): Convergence threshold for stopping the iterations when the PageRank
+e        - convergence_threshold (float, optional): Convergence threshold for stopping the iterations when the PageRank
                                                 scores stabilize. Defaults to 1e-6.
 
         Returns:
         - dict: A dictionary containing PageRank scores for each page in the input graph.
         """
 
-        # Initialization of PageRank scores
-        pr = {page: 1 / len(graph) for page in graph}
+        # Initialization of PageRank scors
+        pr = {page: 1 / len(self.graph) for page in self.graph}
 
         for _ in range(max_iterations):
             pr_new = {}
-            for page in graph:
+            for page in self.graph:
                 # Calculate the new PageRank score
-                pr_new[page] = (1 - d) + d * sum(pr[link] / len(graph[link]) for link in graph if page in graph[link])
+                pr_new[page] = (1 - d) + d * sum(pr[link] / len(self.graph[link]) for link in self.graph if page in self.graph[link])
 
             # Check for convergence
-            if all(abs(pr_new[page] - pr[page]) < convergence_threshold for page in graph):
+            if all(abs(pr_new[page] - pr[page]) < convergence_threshold for page in self.graph):
                 break
 
             pr = pr_new
